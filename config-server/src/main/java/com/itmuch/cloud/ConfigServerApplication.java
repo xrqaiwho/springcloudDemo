85e2178ba3fc1002570e903d3f36b7894f02626f
13005438848@163.com <13005438848@163.com>
2019-10-14 17:31:43 +0800

    测试configserver

--- config-server/src/main/java/com/itmuch/cloud/ConfigServerApplication.java
+++ config-server/src/main/java/com/itmuch/cloud/ConfigServerApplication.java
@@ -6,10 +6,8 @@ import org.springframework.cloud.config.server.EnableConfigServer;
 
 @SpringBootApplication
 @EnableConfigServer
-public class ConfigServer {
-
-    public static void main(String[] args) {
-
-        SpringApplication.run(ConfigServer.class,args);
-    }
+public class ConfigServerApplication {
+  public static void main(String[] args) {
+    SpringApplication.run(ConfigServerApplication.class, args);
+  }
 }
